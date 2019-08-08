0cf13e8574dcda0a5b5efe9bfdf18ac06c12b7b6
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2019-08-08 05:02:45 +0100

    Exit Dialog added, Screen is not sized on the emulator and add button is not showing the list of tasks.

--- app/src/main/java/com/esq/todolist/MainActivity.java
+++ app/src/main/java/com/esq/todolist/MainActivity.java
@@ -1,5 +1,7 @@
 package com.esq.todolist;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,4 +25,26 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainTodoScreen.class);
         startActivity(intent);
     }
+//Add an exit dialog
+    @Override
+    public void onBackPressed() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("Are you sure you want to do this? ");
+        builder.setCancelable(true);
+        builder.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+        builder.setPositiveButton("Close!", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
 }
+

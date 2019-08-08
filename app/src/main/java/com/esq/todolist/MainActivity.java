89df650d7de390653544125e4afe9c7d62d72f88
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2019-08-08 01:28:00 +0100

    Fixed Manifest Errors and built succesfully by 1:00Am. But the main screen is not sending intents and Screen is not sized on the emulator.

--- app/src/main/java/com/esq/todolist/MainActivity.java
+++ app/src/main/java/com/esq/todolist/MainActivity.java
@@ -1,9 +1,11 @@
 package com.esq.todolist;
 
-import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,22 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        TextView ivWelcome = findViewById(R.id.ivWelcome);
+        ImageView ivWelcome = findViewById(R.id.ivWelcome);
         TextView tvSubWelcome = findViewById(R.id.tvSubWelcome);
         TextView btnget = findViewById(R.id.btnget);
 
-        //Import fonts
+    }
 
-        Typeface MLight = Typeface.createFromAsset(getAssets(),"fonts/MLight");
-        Typeface MMedium = Typeface.createFromAsset(getAssets(),"fonts/MMedium");
-        Typeface MRegular = Typeface.createFromAsset(getAssets(),"fonts/MRegular");
-
-        //Customize fonts
-        ivWelcome.setTypeface(MRegular);
-        tvSubWelcome.setTypeface(MLight);
-        btnget.setTypeface(MRegular);
-
-
+    public void goToMainTodoScreen(View view){
+        Intent intent = new Intent(this, MainTodoScreen.class);
+        startActivity(intent);
     }
 }

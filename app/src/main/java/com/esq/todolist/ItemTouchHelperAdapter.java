3eb32e4d574f5542f7f3380389cd0ff3e0a6ce81
Ebhomenye Emmanuel <eebhomenye@yahoo.com>
2019-08-10 03:38:13 +0100

    Button is not positioned well on the device. New itemTouchHelper interface was implememeted in the adapter.

--- app/src/main/java/com/esq/todolist/ItemTouchHelperAdapter.java
+++ app/src/main/java/com/esq/todolist/ItemTouchHelperAdapter.java
@@ -2,6 +2,6 @@ package com.esq.todolist;
 //Interface added to listen for event callbacks
 public interface ItemTouchHelperAdapter {
 
-    void onItemMove(int fromPosition, int toPosition);
+    boolean onItemMove(int fromPosition, int toPosition);
     void onItemDismiss(int position);
 }
